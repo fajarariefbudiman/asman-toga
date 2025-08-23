bfebd7c6de5a7e989949526d2253bbe28aac3800
dimsard <d.ardiansyah999@gmail.com>
2025-08-23 18:17:22 +0700

    fix env for db

--- config/database.go
+++ config/database.go
@@ -12,11 +12,11 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	user := os.Getenv("DB_USER")
-	pass := os.Getenv("DB_PASS")
+	user := os.Getenv("DB_USERNAME")
+	pass := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
+	name := os.Getenv("DB_DATABASE")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		user, pass, host, port, name)
